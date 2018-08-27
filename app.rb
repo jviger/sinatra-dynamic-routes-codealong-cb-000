640bfb60bba3fc2c6c893889dc766562e8e7567f
Jason Viger <jason.viger@gmail.com>
2018-08-27 09:54:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,7 +29,9 @@ class App < Sinatra::Base
     @user_name = params[:name]
     @num1 = params[:num1]
     @num2 = params[:num2]
-    "#{@num1 * @num2}"
+    @product = @num1 * @num2
+    "#{@product}"
+    erb: '/multiple/'
   end
   
 
